679a1963c36a00c8480613b3a1e21b9ece06d5e0
Aswik <aswik01@gmail.com>
2023-04-10 22:03:05 +0530

    final commit

---
+++ src/components/home.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Portfolio=()=>{
+    return(
+        <div>
+            <h2 className="ui center aligned icon header">
+                <i className="circular users icon"></i>
+                About
+            </h2>
+            <div className="ui contents">
+                <div className="comment">
+                    
+                <div className="content">
+                    <a href="/" className="author">aswik</a>
+                <div className="metadata">
+                    <span className="text">
+                    Saveetha Engineering college,
+                    CSE department
+                    </span>
+                </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Portfolio;
